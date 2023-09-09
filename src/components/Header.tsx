58b9259ccc9756c9f3f1d01c57721a6c8cb7162f
CarlesRojas <carlesrojas@outlook.com>
2023-09-09 23:21:17 +0200

    final header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <header className="fixed top-0 left-0 right-0 z-50 w-screen p-4 flex justify-center">
             <motion.div
-                className="relative w-fit max-w-[calc(100vw_-_2rem)] h-fit border rounded-3xl sm:rounded-full border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.05] backdrop-blur-md dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+                className="relative w-fit max-w-[calc(100vw_-_2rem)] h-fit rounded-3xl sm:rounded-full shadow-lg border border-white dark:border-gray-800 bg-white dark:bg-gray-950 !bg-opacity-75 backdrop-blur-md"
                 initial={{ opacity: 0, y: -100 }}
                 animate={{ opacity: 1, y: -0 }}
             >
@@ -17,7 +17,7 @@ const Header = () => {
                         {links.map((link) => (
                             <li className="flex items-center justify-center relative" key={link}>
                                 <a
-                                    className={`flex items-center justify-center text-sm font-medium px-4 py-3 text-gray-500 hover:text-gray-950 transition dark:text-gray-400 dark:hover:text-gray-200 ${
+                                    className={`flex items-center justify-center font-medium px-4 py-3 text-gray-500 hover:text-gray-950 transition dark:text-gray-400 dark:hover:text-gray-200 ${
                                         activeSection === link ? '!text-gray-950 dark:!text-gray-200' : ''
                                     }`}
                                     href={`#${link}`}
@@ -27,7 +27,7 @@ const Header = () => {
 
                                     {link === activeSection && (
                                         <motion.span
-                                            className="bg-gray-200 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                                            className="absolute inset-0 -z-10 rounded-full bg-gray-200 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 !border-opacity-60"
                                             layoutId="activeSection"
                                             transition={{
                                                 type: 'spring',
