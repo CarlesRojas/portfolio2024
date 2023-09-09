74973d93bb4315f2828ab52a3929144092f2ae5c
CarlesRojas <carlesrojas@outlook.com>
2023-09-09 17:43:30 +0200

    change to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [serviceWorker(), tailwind(), react()],
-  output: "static",
+  output: "hybrid",
   adapter: vercel()
 });
