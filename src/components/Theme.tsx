9affb6a9dccbf39032e8ff80cd410fe29ee15c81
CarlesRojas <carlesrojas@outlook.com>
2023-09-10 21:12:58 +0200

    add theme switch

---
+++ src/components/Theme.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { RiMoonFill, RiSunFill } from 'react-icons/ri';
+
+type Theme = 'light' | 'dark';
+const THEME_KEY = 'carles-rojas-portfolio_theme';
+
+const Theme = () => {
+    const [theme, setThemeInternal] = useState<Theme>('dark');
+
+    const setTheme = (newTheme: Theme, saveToLocalStorage: boolean = false) => {
+        setThemeInternal(newTheme);
+
+        if (saveToLocalStorage) window.localStorage.setItem(THEME_KEY, newTheme);
+
+        newTheme === 'light'
+            ? document.documentElement.classList.remove('dark')
+            : document.documentElement.classList.add('dark');
+    };
+
+    useEffect(() => {
+        const currentTheme: Theme = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
+        setTheme(currentTheme);
+    }, []);
+
+    const onSystemThemeChange = (e: MediaQueryListEvent) => {
+        if (window.localStorage.getItem(THEME_KEY) !== null) return;
+
+        const newSystemTheme = e.matches ? 'dark' : 'light';
+        setTheme(newSystemTheme, false);
+    };
+
+    useEffect(() => {
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', onSystemThemeChange);
+
+        return () => {
+            window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', onSystemThemeChange);
+        };
+    }, []);
+
+    return (
+        <button
+            className="fixed z-40 bottom-6 right-6 p-4 w-14 h-14 rounded-full flex items-center justify-center outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all border border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-950 !bg-opacity-75 backdrop-blur-md shadow-sm"
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light', true)}
+        >
+            {theme === 'light' ? <RiSunFill className="w-full h-full" /> : <RiMoonFill className="w-full h-full" />}
+        </button>
+    );
+};
+
+export default Theme;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme'); // eslint-disable-line
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
     theme: {
         extend: {
             sans: {
@@ -15,6 +16,8 @@ module.exports = {
             },
         },
     },
+
+    darkMode: 'class',
 
     plugins: [require('@savvywombat/tailwindcss-grid-areas')],
 };
