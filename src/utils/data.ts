003c64189417a4578a5275d034986426483c0eef
CarlesRojas <carlesrojas@outlook.com>
2023-09-10 09:22:00 +0200

    add actions to home section

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,27 @@
 export const links = ['Home', 'Apps', 'Games', 'Skills', 'Experience'] as const;
-
 export type LinkId = (typeof links)[number];
+
+export type HomeAction = {
+    link: string;
+    icon: string;
+    title?: string;
+};
+export const homeActions: HomeAction[] = [
+    {
+        link: 'assets/CarlesRojas_SoftwareDeveloper_CV.pdf',
+        icon: 'ri:file-download-fill',
+        title: 'Download CV',
+    },
+    {
+        link: 'https://www.linkedin.com/in/carles-rojas/',
+        icon: 'ri:linkedin-fill',
+    },
+    {
+        link: 'https://github.com/CarlesRojas',
+        icon: 'ri:github-fill',
+    },
+    {
+        link: 'mailto:email@carlesrojas.com',
+        icon: 'ri:mail-fill',
+    },
+];
